0e9d92ae2a6239e6efb7616d6bb5efc07a901189
Nickita <nickita.seomin@yandex.ru>
2022-06-19 00:05:27 +0300

    add gitActions for nodejs

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ let cors = require('cors')
 const corsOptions ={
     origin:'http://localhost:3000',
     credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200 
+    optionSuccessStatus:200
 }
 
-app.use(express.json());
+app.use(express.jsonnn());
 app.use(cors(corsOptions));
 
 
